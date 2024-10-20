3da9e3a67ba453f2484741412d191f85fdc6cdb9
Yc3t <ycetaibi@gmail.com>
2024-10-20 23:03:15 +0200

    added search movies functionality with mockup

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Movies } from './components/Movies'
+import { useMovies } from './hooks/useMovies'
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const { movies: mappedMovies } = useMovies()
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='m1'>
+        <header>
+          <h1 className='title'>Movie Finder</h1>
+
+          <form className="form1">
+            <input placeholder="Batman,Star Wars..." />
+            <button>Search</button>
+
+          </form>
+        </header>
+        <main>
+          <Movies movies={mappedMovies} />
+        </main>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
